e09c155350d02bce884aadf9d1ebb53dbb696b8c
Matheus Bione <mathenbiolis@gmail.com>
2022-08-17 12:36:45 -0300

    Código fonte traduzido, indentado, funcional e suportando até duas casas decimais

--- main.c
+++ main.c
@@ -1,27 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Suporte de até duas casas decimais
+
 int main() { 
-  float numero1, numero2;
-  char operator;
+    float numero1, numero2;
+    char operador;
 
-  printf("List of Operators: + (add) - (subtract) / (divide) * (multiply)\n");
-  printf("Enter Number One: ");
-  scanf("%f", &numero1);
-  printf("Enter Operator: ");
-  scanf(" %c", &operator);
-  printf("Enter Number Two: ");
-  scanf("%lf", &numero2);
+    printf("A calculadora suporta números com precisão de até duas casas decimais\n");
+    printf("Lista de operações: + (soma), - (subtração), * (multiplicação) e / (divisão)\n");
+    printf("Digite o primeiro número: ");
+    scanf("%f", &numero1);
 
-  if (operator == '+') {
-    printf("%f\n", numero1 + numero);
-  } else if (operator== '-') {
-    printf("%f\n", numero1 - numero2);
-  } else if (operator== '/') {
-    printf("%f\n", numero1 / numero2);
-  } else if (operator== '*') {
-    printf("%f\n", numero1 * numero2);
-  } else {
-    printf("You have not inputted a valid operator!");
-  }
+    printf("Insira o operador: ");
+    // Espaço antes do "%c" para eliminar espaços e backspaces do input (buffer do último input)
+    scanf(" %c", &operador);
+
+    printf("Digite o segundo número: ");
+    scanf("%f", &numero2);
+
+    printf("O resultado é: ");
+
+    if (operador == '+') {
+	printf("%.2f", numero1 + numero2);
+    } else if (operador == '-') {
+	printf("%.2f", numero1 - numero2);
+    } else if (operador == '*') {
+	printf("%.2f", numero1 * numero2);
+    } else if (operador == '/') {
+	printf("%.2f", numero1 / numero2);
+    }
+
+    printf("\n");
+
+    return 0;
 }
