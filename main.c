774286aa20792bfb3f958a418067973e1618f497
Matheus Bione <mathenbiolis@gmail.com>
2022-08-17 13:00:31 -0300

    Adicionada implementação que impede o usuário de fazer uma operação com um operador inválido e o pede novamente

--- main.c
+++ main.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // Suporte de até duas casas decimais
 
-int main() { 
+int main() {   
     float numero1, numero2;
     char operador;
+    char* operadores_disponiveis = "+-*/";
 
     printf("A calculadora suporta números com precisão de até duas casas decimais\n");
     printf("Lista de operações: + (soma), - (subtração), * (multiplicação) e / (divisão)\n");
     printf("Digite o primeiro número: ");
     scanf("%f", &numero1);
 
-    printf("Insira o operador: ");
-    // Espaço antes do "%c" para eliminar espaços e backspaces do input (buffer do último input)
-    scanf(" %c", &operador);
+    while (1) {
+	printf("Insira o operador: ");
+	// Espaço antes do "%c" para eliminar espaços e backspaces do input (buffer do último input)
+	scanf(" %c", &operador);
+    	
+	if (strchr((operadores_disponiveis), operador) != NULL) {
+	    break;
+	} else {
+	    printf("Operador inválido\n");
+	}
+    }
 
     printf("Digite o segundo número: ");
     scanf("%f", &numero2);
