52dad3a0133898a1f627eec1fe2f784ad7e97cc1
Matheus Bione <mathenbiolis@gmail.com>
2022-08-17 10:18:12 -0300

    Renomeadas variáveis que representam os números

--- main.c
+++ main.c
@@ -2,25 +2,25 @@
 #include <stdlib.h>
 
 int main() { 
-  double option1, option2;
+  float numero1, numero2;
   char operator;
 
   printf("List of Operators: + (add) - (subtract) / (divide) * (multiply)\n");
   printf("Enter Number One: ");
-  scanf("%lf", &option1);
+  scanf("%f", &numero1);
   printf("Enter Operator: ");
   scanf(" %c", &operator);
   printf("Enter Number Two: ");
-  scanf("%lf", &option2);
+  scanf("%lf", &numero2);
 
-  if (operator== '+') {
-    printf("%f\n", option1 + option2);
+  if (operator == '+') {
+    printf("%f\n", numero1 + numero);
   } else if (operator== '-') {
-    printf("%f\n", option1 - option2);
+    printf("%f\n", numero1 - numero2);
   } else if (operator== '/') {
-    printf("%f\n", option1 / option2);
+    printf("%f\n", numero1 / numero2);
   } else if (operator== '*') {
-    printf("%f\n", option1 * option2);
+    printf("%f\n", numero1 * numero2);
   } else {
     printf("You have not inputted a valid operator!");
   }
