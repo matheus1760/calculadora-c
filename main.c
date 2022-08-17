cbc97ae905a914ec65a6b3b2e54b06f37d3ec181
Matheus Bione <mathenbiolis@gmail.com>
2022-08-17 10:15:41 -0300

    Retirados comentários antigos

--- main.c
+++ main.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-  // Creating variables
-  double option1;
-  double option2;
+int main() { 
+  double option1, option2;
   char operator;
 
-  // Getting input
   printf("List of Operators: + (add) - (subtract) / (divide) * (multiply)\n");
   printf("Enter Number One: ");
   scanf("%lf", &option1);
@@ -15,8 +12,6 @@ int main() {
   scanf(" %c", &operator);
   printf("Enter Number Two: ");
   scanf("%lf", &option2);
-
-  // Evaluating input for an answer
 
   if (operator== '+') {
     printf("%f\n", option1 + option2);
